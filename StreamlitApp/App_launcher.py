26221c093afaef1b225084a69be7850e9d0524ed
Alder998 <davide.ontano@yahoo.com>
2025-12-28 23:48:36 +0100

    1.3: Changed the "//" with "/" for MacOS Execution

--- StreamlitApp/App_launcher.py
+++ StreamlitApp/App_launcher.py
@@ -21,10 +21,10 @@ st.markdown(
 )
 
 pages = [
-     st.Page("Pages\\Home.py", icon="🌱", default=True, title="Il tuo Orto"),
-     st.Page("Pages\\Diary.py", icon="🖊️", title="Diario"),
-     st.Page("Pages\\Purchase.py", icon="💰", title="Diario Acquisti"),
-     st.Page("Pages\\Dataset.py", icon="🧮", title="Dataset"),
+     st.Page("Pages/Home.py", icon="🌱", default=True, title="Il tuo Orto"),
+     st.Page("Pages/Diary.py", icon="🖊️", title="Diario"),
+     st.Page("Pages/Purchase.py", icon="💰", title="Diario Acquisti"),
+     st.Page("Pages/Dataset.py", icon="🧮", title="Dataset"),
 ]
 
 nav = st.navigation(pages, position="sidebar")
